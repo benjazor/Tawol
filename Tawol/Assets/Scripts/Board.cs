7396cb6812ad409bd8912381ef50c4960fe41bbc
Benjazor <benjazor@gmail.com>
2019-08-17 11:04:12 +0200

    Fonctions Display Board + Modèles 3D

---
+++ Tawol/Assets/Scripts/Board.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public GameObject cube;
+    public Material black;
+    public Material white;
+
+    void BuildBoard()
+    {
+        for (int i = 0; i < 64; i++)
+        {
+            GameObject t_cube = Instantiate(cube);
+            t_cube.transform.position = new Vector3(i/8, 0, i%8);
+            if (i / 8 % 2 == 0)
+            {
+                t_cube.GetComponent<MeshRenderer>().material = (i % 2 == 0 ? white : black);
+            }
+            else
+            {
+                t_cube.GetComponent<MeshRenderer>().material = (i % 2 == 0 ? black : white);
+            }
+
+
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BuildBoard();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
